63744655487c6ef6a8731f09f6887b674ab0ab5c
sj <sj@gmail.com>
2016-08-28 14:41:13 -0700

    Eureka Server

---
+++ src/main/java/com/example/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication eurekaServer=new SpringApplication(EurekaServerApplication.class);
+		eurekaServer.addListeners(new ApplicationPidFileWriter("eureka-server.pid"));
+		eurekaServer.run();
+	}
+}
